a2ceaed5f8d149e0498426d98a13af6b9204f487
Paweł Wilczyński <pawel@livepeer.org>
2025-05-28 12:48:51 +0200

    Make the code more pythonic

--- lib/Contract.py
+++ lib/Contract.py
@@ -1,110 +1,136 @@
 # Any functions which requires reading/writing smart contracts gets dumped here
 # Also connects to the RPC provider and holds accessors to the smart contracts
-import web3 #< Currency conversions
-import sys #< To exit the program
-import json #< Parse JSON ABI file
-# Import our own libraries
-from lib import Util, State
+
+import web3  # Currency conversions
+import json
+import logging
+from lib import State
+from lib.Util import getPrivateKey, getChecksumAddr
+
+
+log = logging.getLogger(__name__)
+
 
 BONDING_CONTRACT_ADDR = '0x35Bcf3c30594191d53231E4FF333E8A770453e40'
 TICKET_BROKER_CONTRACT_ADDR = '0xa8bB618B1520E284046F3dFc448851A1Ff26e41B'
 
 
-### Define contracts
+# Define contracts
 
+def getABI(path) -> dict:
+    """
+    Returns a dict with ABI data
 
-"""
-@brief Returns a JSON object of ABI data
-@param path: absolute/relative path to an ABI file
-"""
-def getABI(path):
+    :param str path: absolute/relative path to an ABI file
+    """
     try:
         with open(path) as f:
             info_json = json.load(f)
             return info_json["abi"]
-    except Exception as e:
-        Util.log("Fatal error: Unable to extract ABI data: {0}".format(e), 1)
-        sys.exit(1)
+    except Exception:
+        log.exception("Fatal error: Unable to extract ABI data")
+        exit(1)
+
 
 abi_bonding_manager = getABI(State.SIPHON_ROOT + "/contracts/BondingManager.json")
-abi_ticket_broker= getABI(State.SIPHON_ROOT + "/contracts/TicketBroker.json")
-# connect to L2 rpc provider
-provider = web3.HTTPProvider(State.L2_RPC_PROVIDER)
-w3 = web3.Web3(provider)
-assert w3.is_connected()
-# prepare contracts
-bonding_contract = w3.eth.contract(address=BONDING_CONTRACT_ADDR, abi=abi_bonding_manager)
-ticket_broker_contract = w3.eth.contract(address=TICKET_BROKER_CONTRACT_ADDR, abi=abi_ticket_broker)
+abi_ticket_broker = getABI(State.SIPHON_ROOT + "/contracts/TicketBroker.json")
 
 
-### Orchestrator ETH logic
+def w3_contracts():
+    # connect to L2 rpc provider
+    provider = web3.HTTPProvider(State.L2_RPC_PROVIDER)
+    w3 = web3.Web3(provider)
+    assert w3.is_connected(), "Unable to connect to the L2 RPC provider"
 
-def pendingFees():
+    # prepare contracts
+    bonding_contract = w3.eth.contract(address=BONDING_CONTRACT_ADDR, abi=abi_bonding_manager)
+    ticket_broker_contract = w3.eth.contract(address=TICKET_BROKER_CONTRACT_ADDR, abi=abi_ticket_broker)
+    return bonding_contract, ticket_broker_contract
+
+class Orchestrator:
+
+    def __init__(self, obj):
+        # Orch details
+        self.source_address = obj.source_address
+        # Get private key
+        self.source_private_key = getPrivateKey(obj.source_key, obj.source_password)
+        # If the password was set via file or environment var but failed to decrypt, exit
+        if not self.source_private_key:
+            log.error("Fatal error: Unable to decrypt keystore file. Exiting...")
+            exit(1)
+        self.source_checksum_address = getChecksumAddr(obj.source_address)
+        # Set target adresses
+        self.target_address = obj.target_address
+        self.target_checksum_address = getChecksumAddr(obj.target_address)
+
+
+def pendingFees(bonding_contract, orch: Orchestrator) -> float:
     try:
-        pending_wei = bonding_contract.functions.pendingFees(State.orchestrator.source_checksum_address, 99999).call()
-        pending_eth = web3.Web3.from_wei(pending_wei, 'ether')
-        return pending_eth
-    except Exception as e:
-        Util.log("Unable to get pending fees: '{0}'".format(e), 1)
+        pending_wei = bonding_contract.functions.pendingFees(orch.source_checksum_address, 99999).call()
+        return web3.Web3.from_wei(pending_wei, 'ether')
+    except Exception:
+        log.warning("Unable to get pending fees", exc_info=True)
         return 0.0
 
-def doWithdrawFees(toTarget):
+
+def doWithdrawFees(bonding_contract, orch: Orchestrator) -> None:
     try:
         # We take a little bit off due to floating point inaccuracies causing tx's to fail
-        transfer_amount = web3.Web3.to_wei(float(State.orchestrator.balance_ETH_pending) - 0.00001, 'ether')
-        receiver_address = State.orchestrator.source_checksum_address
-        Util.log("Withdrawing {0} WEI to {1}".format(transfer_amount, State.orchestrator.source_address), 1)
+        transfer_amount = web3.Web3.to_wei(float(orch.balance_ETH_pending) - 0.00001, 'ether')
+        receiver_address = orch.source_checksum_address
+        log.info("Withdrawing %s WEI to %s", transfer_amount, orch.source_address)
         # Build transaction info
         transaction_obj = bonding_contract.functions.withdrawFees(receiver_address, transfer_amount).build_transaction(
             {
-                "from": State.orchestrator.source_checksum_address,
+                "from": orch.source_checksum_address,
                 'maxFeePerGas': 2000000000,
                 'maxPriorityFeePerGas': 1000000000,
-                "nonce": w3.eth.get_transaction_count(State.orchestrator.source_checksum_address)
+                "nonce": bonding_contract.w3.eth.get_transaction_count(orch.source_checksum_address)
             }
         )
         # Sign and initiate transaction
-        signed_transaction = w3.eth.account.sign_transaction(transaction_obj, State.orchestrator.source_private_key)
-        transaction_hash = w3.eth.send_raw_transaction(signed_transaction.raw_transaction)
-        Util.log("Initiated transaction with hash {0}".format(transaction_hash.hex()), 2)
+        signed_transaction = bonding_contract.w3.eth.account.sign_transaction(transaction_obj, orch.source_private_key)
+        transaction_hash = bonding_contract.w3.eth.send_raw_transaction(signed_transaction.raw_transaction)
+        log.info("Initiated transaction with hash %s", transaction_hash.hex())
         # Wait for transaction to be confirmed
-        w3.eth.wait_for_transaction_receipt(transaction_hash)
-        Util.log('Withdraw fees success.', 2)
-    except Exception as e:
-        Util.log("Unable to withdraw fees: '{0}'".format(e), 1)
+        bonding_contract.w3.eth.wait_for_transaction_receipt(transaction_hash)
+        log.info('Withdraw fees success.')
+    except Exception:
+        log.exception("Unable to withdraw fees")
 
 
-def getEthBalance():
+def getEthBalance(w3, orch: Orchestrator) -> float:
     try:
-        balance_wei = w3.eth.get_balance(State.orchestrator.source_checksum_address)
+        balance_wei = w3.eth.get_balance(orch.source_checksum_address)
         balance_ETH = web3.Web3.from_wei(balance_wei, 'ether')
         return balance_ETH
-    except Exception as e:
-        Util.log("Unable to get ETH balance: '{0}'".format(e), 1)
+    except Exception:
+        log.exception("Unable to get ETH balance")
 
-def doFundDeposit(amount):
+
+def doFundDeposit(ticket_broker_contract, orch: Orchestrator, amount) -> None:
     try:
-        receiver_address = State.orchestrator.target_checksum_address
-        Util.log("Sending deposit {0} WEI directly to receiver's deposit {1}".format(amount, State.orchestrator.target_address), 2)
+        receiver_address = orch.target_checksum_address
+        log.info("Sending deposit %s WEI directly to receiver's deposit %s", amount, orch.target_address)
         amount_wei = web3.Web3.to_wei(amount, 'ether')
         # Build transaction info
         transaction_obj = ticket_broker_contract.functions.fundDepositAndReserveFor(receiver_address, amount_wei, 0).build_transaction(
             {
-                "from": State.orchestrator.source_checksum_address,
+                "from": orch.source_checksum_address,
                 'maxFeePerGas': 2000000000,
                 'maxPriorityFeePerGas': 1000000000,
                 'value': amount_wei,
-                "nonce": w3.eth.get_transaction_count(State.orchestrator.source_checksum_address),
+                "nonce": ticket_broker_contract.w3.eth.get_transaction_count(orch.source_checksum_address),
                 'gas': 300000,
                 'chainId': 54321
             }
         )
         # Sign and initiate transaction
-        signed_transaction = w3.eth.account.sign_transaction(transaction_obj, State.orchestrator.source_private_key)
-        transaction_hash = w3.eth.send_raw_transaction(signed_transaction.raw_transaction)
-        Util.log("Initiated transaction with hash {0}".format(transaction_hash.hex()), 2)
+        signed_transaction = ticket_broker_contract.w3.eth.account.sign_transaction(transaction_obj, orch.source_private_key)
+        transaction_hash = ticket_broker_contract.w3.eth.send_raw_transaction(signed_transaction.raw_transaction)
+        log.info("Initiated transaction with hash %s", transaction_hash.hex())
         # Wait for transaction to be confirmed
-        w3.eth.wait_for_transaction_receipt(transaction_hash)
-        Util.log('Fund deposit success.', 2)
-    except Exception as e:
-        Util.log("Unable to fund deposit: '{0}'".format(e), 1)
+        ticket_broker_contract.w3.eth.wait_for_transaction_receipt(transaction_hash)
+        log.info("Fund deposit success.")
+    except Exception:
+        log.exception("Unable to fund deposit")
--- lib/State.py
+++ lib/State.py
@@ -1,8 +1,8 @@
 # All classes and variables we want to share across files
 # Also parses the config on initialisation
-import configparser #< Parse the .ini file
-import os #< Used to get environment variables & for resolving relative file paths
-
+import configparser
+import os
+import logging
 
 # Determine root directory
 SIPHON_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -11,16 +11,17 @@ if SIPHON_ROOT.endswith("/lib"):
     SIPHON_ROOT = os.path.dirname(SIPHON_ROOT)
 
 
-### Config & Variables
+# Config & Variables
 
 
 # Turn config options into a nice object to work with later
 class OrchConf:
-  def __init__(self, key, pw, pub, eth_target):
-    self._source_key = key
-    self._source_password = pw
-    self._source_address = pub
-    self._target_address = eth_target
+    def __init__(self, key, pw, pub, eth_target):
+        self.source_key = key
+        self.source_password = pw
+        self.source_address = pub
+        self.target_address = eth_target
+
 
 # Load config file
 config = configparser.ConfigParser()
@@ -30,31 +31,21 @@ config.read(os.path.join(SIPHON_ROOT, 'config.ini'))
 KEYSTORE_CONFIGS = []
 # If there's no environment variable set, read keystore config from .ini file
 if os.getenv('KEYSTORE', "") == "":
-    for section in config.sections():
-        if section.startswith('keystore'):
-            KEYSTORE_CONFIGS.append(
-                OrchConf(
-                    config[section]['keystore'],
-                    config[section]['password'],
-                    config[section]['source_address'],
-                    config[section]['receiver_address'],
-                )
-            )
+    for name, section in config.items():
+        if not name.startswith('keystore'):
+            continue
+        KEYSTORE_CONFIGS.append(
+            OrchConf(section['keystore'], section['password'], section['source_address'], section['receiver_address']))
+
 else:
     # Else ignore keystore config - read all data from environment variables
     keystores = os.getenv('SIPHON_KEYSTORES', "")
     passwords = os.getenv('SIPHON_PASSWORDS', "")
-    source_adresses = os.getenv('SIPHON_SOURCES', "")
+    source_addresses = os.getenv('SIPHON_SOURCES', "")
     receiver_addresses_eth = os.getenv('SIPHON_TARGETS_ETH', "")
-    for keystore, password, source_adress, receiver_address in zip(keystores, passwords, source_adresses, receiver_addresses_eth):
-            KEYSTORE_CONFIGS.append(
-                OrchConf(
-                    keystore,
-                    password,
-                    source_adress,
-                    receiver_address
-                )
-            )
+    for keystore, password, source_address, receiver_address in zip(keystores, passwords, source_addresses, receiver_addresses_eth):
+        KEYSTORE_CONFIGS.append(OrchConf(keystore, password, source_address, receiver_address))
+
 # Thresholds
 ETH_THRESHOLD = float(os.getenv('SIPHON_ETH_THRESHOLD', config['thresholds']['eth_threshold']))
 ETH_MINVAL = float(os.getenv('SIPHON_ETH_MINVAL', config['thresholds']['eth_minval']))
@@ -63,3 +54,5 @@ L2_RPC_PROVIDER = os.getenv('SIPHON_RPC_L2', config['rpc']['l2'])
 # Logging
 LOG_VERBOSITY = int(os.getenv('SIPHON_VERBOSITY', config['other']['verbosity']))
 LOG_TIMESTAMPED = bool(os.getenv('SIPHON_TIMESTAMPED', config.getboolean('other', 'log_timestamped')))
+
+logging.basicConfig(level=LOG_VERBOSITY, format="%(asctime)s %(levelname)s %(message)s")
